7cea5ff90fbe21d86d656ad3e8a02251ed0139fb
Chent Mokeev <chint2184@gmail.com>
2022-02-07 13:44:36 +0600

    Rows and columns

--- main.dart
+++ main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-    const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +15,75 @@ class MyApp extends StatelessWidget {
             'Flutter demo app',
             style: TextStyle(color: Colors.black),
           ),
-          backgroundColor: Colors.white,
           centerTitle: true,
-        ), 
-        body: Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-          color: Colors.grey,
-          child: const Center(
-            child: Text('Body'),
+        ),
+        backgroundColor: Colors.cyan[300],
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(30),
+                    child: Text('1'),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    padding: EdgeInsets.all(40),
+                    child: Text('2'),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    padding: EdgeInsets.all(50),
+                    child: Text('3'),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(30),
+                    child: Text('1'),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    padding: EdgeInsets.all(40),
+                    child: Text('2'),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    padding: EdgeInsets.all(50),
+                    child: Text('3'),
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(30),
+                    child: Text('1'),
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    padding: EdgeInsets.all(40),
+                    child: Text('2'),
+                  ),
+                  Container(
+                    color: Colors.green,
+                    padding: EdgeInsets.all(50),
+                    child: Text('3'),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
